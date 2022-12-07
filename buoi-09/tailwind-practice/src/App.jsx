199c6e15675680ccb8c74ab7de3eb29463b79d1f
likelionbootcamp3 <likelionbootcamp3@gmail.com>
2022-12-07 16:41:21 +0700

    Buoi 9

--- buoi-09/tailwind-practice/src/App.jsx
+++ buoi-09/tailwind-practice/src/App.jsx
@@ -1,3 +1,5 @@
+import Card from "./components/Card";
+import CardSample from "./components/CardSample";
 import Navbar from "./components/Navbar";
 import NavbarSample from "./components/NavbarSample";
 
@@ -7,6 +9,22 @@ const App = () => {
       <NavbarSample />
       <hr />
       <Navbar />
+      <hr />
+      <CardSample />
+      <hr />
+      <Card />
+
+      <hr />
+      {/* Container */}
+      <div className="max-w-screen-xl mx-auto px-4">
+        {/* Layout: Grid or Flex  */}
+        <div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-4">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
